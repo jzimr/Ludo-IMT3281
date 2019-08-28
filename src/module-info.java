89ad532a3b1831a23e3b6e870bd9453aa9d73d2b
Øivind Kolloen <oeivind.kolloen@ntnu.no>
2019-08-28 05:55:03 +0000

    module-info.java edited online with Bitbucket

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module no.ntnu.imt3281.sudoku {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.logging;
+    requires java.sql;
 
     opens no.ntnu.imt3281.sudoku to javafx.fxml;
     exports no.ntnu.imt3281.sudoku;
